4205a92948b4191cd2fe7345f4c97f14013f1834
Mike Espera <maespera@addu.edu.ph>
2021-02-03 12:28:45 +0900

    Added homepage

---
+++ js/manga-update.js
@@ -0,0 +1,33 @@
+class MangaUpdates {
+  constructor() {
+    const list = $(".manga .links");
+    this.getUpdates().then((updates) => {
+      updates.forEach((obj) => {
+        let li = document.createElement("li");
+        let el = document.createElement("a");
+        el.href = obj.link;
+        el.innerHTML = obj.title;
+        li.appendChild(el);
+        list.appendChild(li);
+      });
+    });
+  }
+
+  async getUpdates() {
+    let list = [];
+    await fetch(
+      "https://mangadex.org/rss/follows/ZpmV7dFu5rKzfS3se6RhaEXg8cYwMtHn?h=0"
+    )
+      .then((res) => res.text())
+      .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+      .then((data) => {
+        list = Array.from(data.querySelectorAll("item")).map((node) => {
+          return {
+            title: node.children[0].innerHTML,
+            link: node.children[1].innerHTML,
+          };
+        });
+      });
+    return list;
+  }
+}
